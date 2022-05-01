fc0e3941ace0f3982dd548fb8dda8ce922504fb3
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-01 22:07:16 +0600

    home-page item limited to 6

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.port || 5000;
-
 
 //!------- middle-wire -------
 app.use(express.json());
@@ -15,36 +13,44 @@ app.use(cors());
 //username: furnitureHouse
 //Pass : Akc6Exfk7ME33LCk
 
-
-
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.3hz1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const itemCollection = client.db("furnitureHouse").collection("items");
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db("furnitureHouse").collection("items");
 
-        //!------ Show all Items in HomePage ------
-        app.get("/inventory", async(req, res)=>{
-            const cursor = itemCollection.find({});
-            const items = await cursor.toArray();
-            res.send(items);
-        })
-    }
-    finally{
-        // await client.close();
-    }
+    //!------ Show all Items ------
+    app.get("/inventory", async (req, res) => {
+      const size = parseInt(req.query.itemSize);
+      const cursor = itemCollection.find({});
+      let items;
+
+      if (size) {
+        items = await cursor.limit(size).toArray();
+      } else {
+        items = await cursor.toArray();
+      }
+
+      res.send(items);
+    });
+  } 
+  finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Warehouse Management Server");
+  res.send("Warehouse Management Server");
 });
 
-app.listen(port, ()=>{
-    console.log("listening from port: ", port);
-})
+app.listen(port, () => {
+  console.log("listening from port: ", port);
+});
